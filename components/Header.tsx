7486761a561b4286c9fccc5ee2cc99e318aee807
k4ysam <ksamaksh0601@gmail.com>
2026-04-07 16:53:55 -0400

    fix: center the header title and restore header scale

--- components/Header.tsx
+++ components/Header.tsx
@@ -56,6 +56,9 @@ export function Header() {
             <span />
             <span />
           </button>
+        </div>
+
+        <div className="header-center">
           <div className="logo">
             debugdle<span className="cursor">_</span>
           </div>
